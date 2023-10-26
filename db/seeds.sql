a756f3fea8a397709ec998314065a504020c2b8d
G303K <micder18@gmail.com>
2023-10-26 16:57:42 -0400

    Fixed ID issue in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,12 +9,12 @@ VALUES ('Maintenance'),
 -- DROP TABLE IF EXISTS role;
 INSERT INTO role (title, salary, department_id)
 VALUES
-    ('Sales Lead', '100000', 1),
-    ('Salesperson', '80000', 1),
-    ('Software Engineer', '120000', 2);
+    ('Maintenace Tech', '30000', 1),
+    ('Office Staff', '32000', 2),
+    ('Community Manager', '40000', 3);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, null),
-    ('Whitney', 'Kropat', 2, 1),
-    ('Another', 'Person', 3, 1);
+    ('John', 'Dawson', 1, 3),
+    ('Whitney', 'Hart', 2, 3),
+    ('Billy', 'Johnson', 3, NULL);
