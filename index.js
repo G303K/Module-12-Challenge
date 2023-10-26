83238df4654389d8d487fa5c7a961d5981209c2e
G303K <micder18@gmail.com>
2023-10-26 17:01:07 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const db = mysql.createConnection(
   {
     host: "localhost",
     user: "root",
-    password: "p@ssword",
+    password: "2515",
     database: "employees_db",
   },
   console.log(`Connected to the employees_db database.`)
